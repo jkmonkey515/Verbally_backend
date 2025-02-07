d5e8635bf1299854ed5f9449f839cf78551b2f68
JK <jkmonkey515@gmail.com>
2025-02-07 08:31:00 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with credentials
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true, // Enable credentials
+  });
+
   await app.listen(3000);
 }
 bootstrap();
